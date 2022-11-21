8ecf300bea06d63de2ed0701be40129218a82a85
June <90249137+GooseBug@users.noreply.github.com>
2022-11-21 20:19:47 +0900

    Update basic_DICOM_code.py

--- basic_DICOM_code.py
+++ basic_DICOM_code.py
@@ -1,9 +1,15 @@
-import numpy as np
+import os, re
 from typing import Union
 
 import pydicom
 from pydicom import dcmread, uid
 from pydicom.pixel_data_handlers import util
+
+
+import numpy as np
+import matplotlib.pyplot as plt
+
+from IPython.display import clear_output
 
 
 
@@ -44,3 +50,51 @@ def min_max_scaling(np_array: np.ndarray) -> np.ndarray:
     max_score = np.max(np_array)
     
     return (np_array-min_score)/(max_score-min_score)
+
+
+        
+        
+class dicom_viewer_by_dcm_directory:
+    """
+    dcm_directory_path에 있는 모든 DICOM 파일들을 순서대로 보여준다.
+    """
+    def __init__(self, dcm_directory_path: str):
+        
+        self.dcm_directory_path = dcm_directory_path
+
+    
+    def process(self):
+        
+        dcm_file_list = self.get_dcm_file_list()
+        dcm_file_list = sorted(dcm_file_list)
+        
+        for dcm_file in dcm_file_list:
+            
+            dcm_file_path = f"{self.dcm_directory_path}/{dcm_file}"
+            dcm_array, _ = get_dicom_metaData_and_image(dcm_file_path)
+            dcm_array = min_max_scaling(dcm_array)
+            
+            plt.figure(figsize=(12, 12))
+            plt.imshow(dcm_array, cmap='gray')
+            plt.show()
+            clear_output(wait=True)
+    
+    
+    
+
+    def get_dcm_file_list(self):
+
+        # file의 목록들을 다 가지고 온다.
+        file_list = os.listdir(self.dcm_directory_path)
+
+        dcm_list = []
+
+        for oneFileName in file_list:
+
+            end_is_point_dcm = len(re.findall("[.]dcm$", oneFileName))
+
+            if end_is_point_dcm == 1:
+
+                dcm_list.append(oneFileName)
+
+        return dcm_list
