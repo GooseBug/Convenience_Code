5620a6f102155603ecdf7f4d7dd47b0f6b2a2754
June <90249137+GooseBug@users.noreply.github.com>
2022-11-23 18:12:35 +0900

    Update basic_DICOM_code.py

--- basic_DICOM_code.py
+++ basic_DICOM_code.py
@@ -14,54 +14,24 @@ from IPython.display import clear_output
 
 
 
-def get_dicom_metaData_and_image(
-    dcm_path: str
-) -> Union[np.ndarray, pydicom.dataset.FileDataset]:
-    """
-    DICOM으로부터 필요한 데이터를 가지고 온다.
-    ----------------------------------------------------------------
-    이미지 데이터는 Modality LUT, VOI LUT 하여 가지고 온다.
-    """
-    # DICOM Object를 가지고 온다.
-    dcmObject = dcmread(dcm_path)
-    
-    # TransferSyntaxUID가 설정되어 있다면, pixel array를 바로 가지고 온다.
-    try:
-        arr = dcmObject.pixel_array
-    except:
-        # pixel_array를 가져오기 전에 파일을 읽은 후 TransferSyntaxUID 설정
-        dcmObject.file_meta.TransferSyntaxUID = uid.ImplicitVRLittleEndian  # or whatever is the correct transfer syntax for the file
-        arr = dcmObject.pixel_array
-        
-    # Apply rescale operation (if required, otherwise returns arr unchanged)
-    rescaled_arr = util.apply_modality_lut(arr, dcmObject)
-    # Apply windowing operation (if required, otherwise returns arr unchanged)
-    img = util.apply_voi_lut(rescaled_arr, dcmObject, index=0)
-        
-    return img, dcmObject
-
-
-
-
-def min_max_scaling(np_array: np.ndarray) -> np.ndarray:
-    """
-    np array에 대하여 min-max scaling 한다.
-    """
-    min_score = np.min(np_array)
-    max_score = np.max(np_array)
-    
-    return (np_array-min_score)/(max_score-min_score)
-
-
-        
-        
 class dicom_Viewer:
 
     def __init__(self, dcm_directory_path: str):
         """
+        설명
+        ----------------------------------------------------------------
+        dicom_Viewer는 DICOM이 들어 있는 디렉터리에 대하여,
+        DICOM을 순서대로 출력한다.
+        ----------------------------------------------------------------
+        
+        
         주요 Method
-        print_sequentially
-        Slide_viewer
+        ----------------------------------------------------------------
+        1) print_sequentially
+        >>> 3D 이미지를 File의 이름 순으로 천천히 출력한다.
+        2) Slide_viewer
+        >>> 3D 이미지를 Slide를 움직여 원하는 형태로 출력한다.
+        ----------------------------------------------------------------
         """
         self.dcm_directory_path = dcm_directory_path
         self.img_arr = None
@@ -206,8 +176,7 @@ class dicom_Viewer:
                 dcm_list.append(oneFileName)
 
         return dcm_list
-    
-    
+
     
     
 class extract_dicom_information:
@@ -282,3 +251,44 @@ class extract_dicom_information:
         except:
             result = self.warning_phrase
         return result
+    
+    
+    
+    
+def get_dicom_metaData_and_image(
+    dcm_path: str
+) -> Union[np.ndarray, pydicom.dataset.FileDataset]:
+    """
+    DICOM으로부터 필요한 데이터를 가지고 온다.
+    ----------------------------------------------------------------
+    이미지 데이터는 Modality LUT, VOI LUT 하여 가지고 온다.
+    """
+    # DICOM Object를 가지고 온다.
+    dcmObject = dcmread(dcm_path)
+    
+    # TransferSyntaxUID가 설정되어 있다면, pixel array를 바로 가지고 온다.
+    try:
+        arr = dcmObject.pixel_array
+    except:
+        # pixel_array를 가져오기 전에 파일을 읽은 후 TransferSyntaxUID 설정
+        dcmObject.file_meta.TransferSyntaxUID = uid.ImplicitVRLittleEndian  # or whatever is the correct transfer syntax for the file
+        arr = dcmObject.pixel_array
+        
+    # Apply rescale operation (if required, otherwise returns arr unchanged)
+    rescaled_arr = util.apply_modality_lut(arr, dcmObject)
+    # Apply windowing operation (if required, otherwise returns arr unchanged)
+    img = util.apply_voi_lut(rescaled_arr, dcmObject, index=0)
+        
+    return img, dcmObject
+
+
+
+
+def min_max_scaling(np_array: np.ndarray) -> np.ndarray:
+    """
+    np array에 대하여 min-max scaling 한다.
+    """
+    min_score = np.min(np_array)
+    max_score = np.max(np_array)
+    
+    return (np_array-min_score)/(max_score-min_score)
